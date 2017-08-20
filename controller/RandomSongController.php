49c4e423176caab38f384c639d331f6bd640969a
w0rmr1d3r <rguimerao@gmail.com>
2017-08-20 22:13:58 +0100

    fixed identation

--- controller/RandomSongController.php
+++ controller/RandomSongController.php
@@ -1,39 +1,36 @@
 <?php
-      define('__ROOT__', '../');
-      define('__VIEW__', __ROOT__.'view/');
-      define('__MODEL__', __ROOT__.'model/');
+    define('__ROOT__', '../');
+    define('__VIEW__', __ROOT__.'view/');
+    define('__MODEL__', __ROOT__.'model/');
 
-      require_once(__MODEL__.'DB.php');
-      require_once(__MODEL__.'Song.php');
+    require_once(__MODEL__.'DB.php');
+    require_once(__MODEL__.'Song.php');
 
-      // song given
-      $DB = DataBase::getInstance();
-      $mySong = $DB->getSongTest();
+    // song given
+    /*
+    $DB = DataBase::getInstance();
+    $mySong = $DB->getSongTest();
 
-      $songTitle = $mySong->getTitle();
-      $songPath = $mySong->getPath();
-      $songFileName = $mySong->getFileName();
-      $songArtist = $mySong->getArtist();
-      $songAlbum = $mySong->getAlbum();
-      $songCategory = $mySong->getCategory();
+    $songTitle = $mySong->getTitle();
+    $songPath = $mySong->getPath();
+    $songFileName = $mySong->getFileName();
+    $songArtist = $mySong->getArtist();
+    $songAlbum = $mySong->getAlbum();
+    $songCategory = $mySong->getCategory();
+    */
 
-      // song given view + download button
-      require_once(__VIEW__.'SongGivenView.php');
+    // Random song with DB
+    $DB = DataBase::getInstance();
+    $mySong = $DB->getRandomSong();
+    $DB->closeConnection();
 
-      /*
-      // Random song with DB
-      $DB = DataBase::getInstance();
-      $mySong = $DB->getRandomSong();
-      $DB->closeConnection();
+    $songTitle = $mySong->getTitle();
+    $songPath = $mySong->getPath();
+    $songFileName = $mySong->getFileName();
+    $songArtist = $mySong->getArtist();
+    $songAlbum = $mySong->getAlbum();
+    $songCategory = $mySong->getCategory();
 
-      $songTitle = $mySong->getTitle();
-      $songPath = $mySong->getPath();
-      $songFileName = $mySong->getFileName();
-      $songArtist = $mySong->getArtist();
-      $songAlbum = $mySong->getAlbum();
-      $songCategory = $mySong->getCategory();
-
-      // song given view + download button
-      require_once(__VIEW__.'SongGivenView.php');
-      */
+    // song given view + download button
+    require_once(__VIEW__.'SongGivenView.php');    
 ?>
