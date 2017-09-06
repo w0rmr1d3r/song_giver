9d60a1ee9c321ccaf982a155feb05b0e4af727e4
w0rmr1d3r <rguimerao@gmail.com>
2017-09-06 11:25:57 +0100

    views code style checked

--- view/SongGivenView.php
+++ view/SongGivenView.php
@@ -1,21 +1,20 @@
-
 <div class="card" id="custom-song-given-div">
     <div class="card-body">
-	    <h4 class="card-title" title="Title"><?php echo $songTitle; ?></h4>
-	    <ul class="list-group">
-		    <li class="list-group-item" title="Artist">
+        <h4 class="card-title" title="Title"><?php echo $songTitle; ?></h4>
+        <ul class="list-group">
+            <li class="list-group-item" title="Artist">
                 <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                 <?php echo $songArtist; ?>
             </li>
-		    <li class="list-group-item" title="Album">
+            <li class="list-group-item" title="Album">
                 <span class="glyphicon glyphicon-cd" aria-hidden="true"></span>
                 <?php echo $songAlbum; ?>
             </li>
-		    <li class="list-group-item" title="Category">
+            <li class="list-group-item" title="Category">
                 <span class="glyphicon glyphicon-music" aria-hidden="true"></span>
                 <?php echo $songCategory; ?>
             </li>
-	    </ul>
+        </ul>
 <?php
     echo '
         <a 
--- view/UploadFileConfirmation.php
+++ view/UploadFileConfirmation.php
@@ -1,2 +1,1 @@
-
 <h1><?php echo $message; ?></h1>
